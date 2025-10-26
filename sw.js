c10b81fb55e04e7fc538fb8f06c338deb93776a1
thoughtleader <email@michaelevan.com>
2025-10-26 11:56:25 -0700

    Bug Fixes

--- sw.js
+++ sw.js
@@ -7,11 +7,9 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
-  console.log('[SW] Installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('[SW] Caching app shell and symbols');
         return cache.addAll(urlsToCache);
       })
       .then(() => self.skipWaiting())
@@ -19,13 +17,11 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[SW] Activating...');
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('[SW] Removing old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
